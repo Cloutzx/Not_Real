2e3e4c64bb44fd02d97b040902acdd6828384bca
Cloutz <123915410+Cloutzx@users.noreply.github.com>
2026-08-02 00:54:01 -0400

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -3,15 +3,44 @@
 ========================================================= */
 
 
+const soundPath = "/sounds/";
+
+
+
 const Sounds = {
 
-    click: new Audio("/sounds/click.mp3"),
-    spin: new Audio("/sounds/spin.mp3"),
-    win: new Audio("/sounds/win.mp3"),
-    lose: new Audio("/sounds/lose.mp3"),
-    fish: new Audio("/sounds/fish.mp3"),
-    reward: new Audio("/sounds/reward.mp3"),
-    coinflip: new Audio("/sounds/coinflip.mp3")
+
+    click: new Audio(soundPath + "click.mp3"),
+
+
+    slotSpin: new Audio(soundPath + "slot-spin.mp3"),
+
+
+    slotStop: new Audio(soundPath + "slot-stop.mp3"),
+
+
+    cardDeal: new Audio(soundPath + "card-deal.mp3"),
+
+
+    cardFlip: new Audio(soundPath + "card-flip.mp3"),
+
+
+    coinFlip: new Audio(soundPath + "coin-flip.mp3"),
+
+
+    fish: new Audio(soundPath + "fish.mp3"),
+
+
+    jackpot: new Audio(soundPath + "jackpot.mp3"),
+
+
+    reward: new Audio(soundPath + "reward.mp3"),
+
+
+    win: new Audio(soundPath + "win.mp3"),
+
+
+    lose: new Audio(soundPath + "lose.mp3")
 
 };
 
@@ -22,6 +51,7 @@ Object.values(Sounds).forEach(sound=>{
     sound.volume = 0.5;
 
 });
+
 
 
 
@@ -37,12 +67,13 @@ function playSound(sound){
 
     Sounds[sound].play().catch(error=>{
 
-        console.log("Sound blocked:", error);
+        console.log("Audio blocked:", error);
 
     });
 
 
 }
+
 
 
 
@@ -57,9 +88,58 @@ window.LuckySounds = {
     },
 
 
-    spin(){
+    slotSpin(){
 
-        playSound("spin");
+        playSound("slotSpin");
+
+    },
+
+
+    slotStop(){
+
+        playSound("slotStop");
+
+    },
+
+
+    cardDeal(){
+
+        playSound("cardDeal");
+
+    },
+
+
+    cardFlip(){
+
+        playSound("cardFlip");
+
+    },
+
+
+    coinFlip(){
+
+        playSound("coinFlip");
+
+    },
+
+
+    fish(){
+
+        playSound("fish");
+
+    },
+
+
+    jackpot(){
+
+        playSound("jackpot");
+
+    },
+
+
+    reward(){
+
+        playSound("reward");
 
     },
 
@@ -75,34 +155,18 @@ window.LuckySounds = {
 
         playSound("lose");
 
-    },
-
-
-    fish(){
-
-        playSound("fish");
-
-    },
-
-
-    reward(){
-
-        playSound("reward");
-
-    },
-
-
-    coinflip(){
-
-        playSound("coinflip");
-
     }
+
 
 };
 
 
 
 
+
+
+
+// Button click sounds
 
 document.addEventListener("DOMContentLoaded",()=>{
 
