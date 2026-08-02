36fdf478a66ea48c3411624f7decc64da40d5a74
Cloutz <123915410+Cloutzx@users.noreply.github.com>
2026-08-02 01:19:58 -0400

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -1,165 +1,30 @@
-/* =========================================================
-   LUCKY LOUNGE CURRENCY
-========================================================= */
-
-
 let coins;
-
 
 
 function loadCoins(){
 
-
-    let saved =
-    localStorage.getItem("coins");
+    let savedCoins = localStorage.getItem("coins");
 
 
-
-    if(saved === null){
-
+    // Only give starter coins if the player has NEVER played
+    if(savedCoins === null){
 
         coins = 1000;
 
-
         localStorage.setItem(
             "coins",
-            coins
+            "1000"
         );
-
 
     }
     else{
 
-
-        coins = Number(saved);
-
+        coins = Number(savedCoins);
 
     }
-
-
 
 }
 
 
 
 loadCoins();
-
-
-
-
-
-
-
-function updateCoins(){
-
-
-    document
-    .querySelectorAll("#coins")
-    .forEach(element=>{
-
-
-        element.textContent =
-        coins.toLocaleString();
-
-
-    });
-
-
-}
-
-
-
-
-
-
-
-function getCoins(){
-
-
-    return coins;
-
-
-}
-
-
-
-
-
-
-
-function addCoins(amount){
-
-
-    coins += amount;
-
-
-    saveCoins();
-
-
-}
-
-
-
-
-
-
-
-function removeCoins(amount){
-
-
-    if(coins < amount){
-
-
-        return false;
-
-
-    }
-
-
-
-    coins -= amount;
-
-
-    saveCoins();
-
-
-
-    return true;
-
-
-}
-
-
-
-
-
-
-
-function saveCoins(){
-
-
-    localStorage.setItem(
-        "coins",
-        coins
-    );
-
-
-    updateCoins();
-
-
-}
-
-
-
-
-
-
-document.addEventListener(
-"DOMContentLoaded",
-()=>{
-
-
-    updateCoins();
-
-
-});
